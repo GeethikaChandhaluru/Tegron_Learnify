d3965a3ccebe2e63c4eb9c7afb07eb29f8c68663
GeethikaChandhaluru <geethikachandhaluru@gmail.com>
2026-05-08 20:22:11 +0530

    changed code

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Tegron Learnify</h1>
+            <p>Learn courses online easily.</p>
+        </div>
+    );
+}
+
+export default Home;
