670be195f0f6ca5dfe969643979c9959c8692f01
GeethikaChandhaluru <geethikachandhaluru@gmail.com>
2026-05-19 16:12:46 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        origin: [
+            "http://localhost:5173",
+            "https://tegronlearnify.vercel.app"
+        ],
         methods: ['GET', 'POST'],
         credentials: true,
     },
